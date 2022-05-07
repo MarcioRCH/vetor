4a38615ec724a0f88506b8fed76edca0ebf8fb76
MarcioRCH <marciorchsilva@gmail.com>
2022-05-06 23:51:02 -0300

    Calculating average height

--- src4/application/Heights.java
+++ src4/application/Heights.java
@@ -1,10 +1,40 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Methods;
+
 public class Heights {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Heights");
+		System.out.print("How many peoples will be entered? ");
+		int n = sc.nextInt();
+		System.out.println(" ");
+		Methods[] vect = new Methods[n];
 
+		for(int i = 0; i < vect.length; i++) {
+			sc.nextLine();
+			System.out.println((i + 1) + "st person data:");
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Age: ");
+			int age = sc.nextInt();
+			System.out.print("Height: ");
+			double height = sc.nextDouble();
+			vect[i] = new Methods(name, age, height);
+			System.out.println(" ");
+		}
+		double sum = 0.0;
+		for(int i = 0; i < vect.length; i++) {
+			sum += vect[i].getHeight();
+		}
+		double avg = sum / n;
+		System.out.println("Average height: " + avg);
+		sc.close();
 	}
-
 }
