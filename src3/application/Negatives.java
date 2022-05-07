a248c73d1e43920af23a9e57f358f06ad01f4dfd
MarcioRCH <marciorchsilva@gmail.com>
2022-05-06 22:40:34 -0300

    Applying logic and terminating the program

--- src3/application/Negatives.java
+++ src3/application/Negatives.java
@@ -1,10 +1,32 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Negatives {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Negatives");
+		System.out.print("How many numbers will you type? ");
+		int n = sc.nextInt();
+		System.out.println(" ");
+		
+		double[] vect = new double[n];
+		
+		for(int i = 0; i < vect.length; i++) {
+			System.out.print("Enter a number: ");
+			vect[i] = sc.nextDouble();
+		}
+		System.out.printf("\nNegatives numbers:\n");
+		for(int i = 0; i < vect.length; i++) {
+			if(vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
+		}		
+		sc.close();
 	}
 
 }
